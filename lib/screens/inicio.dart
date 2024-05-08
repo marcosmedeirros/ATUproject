d8a101a38697c92c885b06fb1080c00b8040ef58
Marcos Medeiros <marcos.medeiros@acad.ufsm.br>
2024-05-08 15:55:00 -0300

    Adicionar e tirar saldo

--- lib/screens/inicio.dart
+++ lib/screens/inicio.dart
@@ -11,6 +11,58 @@ class Inicio extends StatefulWidget {
 }
 
 class _InicioState extends State<Inicio> {
+  int numeroViagens = 10;
+  double saldoConta = 100.0;
+
+  // Função para adicionar uma viagem e subtrair o saldo
+  void adicionarViagem() {
+    setState(() {
+      numeroViagens++;
+      saldoConta -= 2.50;
+    });
+  }
+
+  // Função para subtrair uma viagem e adicionar ao saldo
+  void subtrairViagem() {
+    if (numeroViagens > 0) {
+      setState(() {
+        numeroViagens--;
+        saldoConta += 2.50;
+      });
+    }
+  }
+
+  // Função para zerar o saldo e o número de viagens
+  void zerar() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Você tem certeza?'),
+          content: Text('Isso irá zerar seu saldo e número de viagens.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  saldoConta = 0;
+                  numeroViagens = 0;
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Sim'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   // Função de navegação personalizada
   void navigateToPage(Widget page) {
     Navigator.push(
@@ -27,14 +79,14 @@ class _InicioState extends State<Inicio> {
       ),
       body: Column(
         children: [
-          SizedBox(height: 20),
+          SizedBox(height: 20), // Aumentei o espaçamento superior
           // Logo
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(1), // Diminuí a margem
             child: Image.asset(
               'assets/images/logobus.png',
-              height: 250,
-              width: 200,
+              height: 200,
+              width: 350,
             ),
           ),
           // Menu de Viagens
@@ -49,7 +101,7 @@ class _InicioState extends State<Inicio> {
                   textAlign: TextAlign.center,
                 ),
                 subtitle: Text(
-                  'R\$ 100,00',
+                  'R\$ ${saldoConta.toStringAsFixed(2)}',
                   style: TextStyle(color: Colors.white, fontSize: 24),
                   textAlign: TextAlign.center,
                 ),
@@ -72,7 +124,7 @@ class _InicioState extends State<Inicio> {
                   textAlign: TextAlign.center,
                 ),
                 subtitle: Text(
-                  '10',
+                  numeroViagens.toString(),
                   style: TextStyle(color: Colors.white, fontSize: 24),
                   textAlign: TextAlign.center,
                 ),
@@ -83,6 +135,36 @@ class _InicioState extends State<Inicio> {
                 },
               ),
             ),
+          ),
+          // Botões para adicionar e subtrair viagens
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: adicionarViagem,
+                child: Text('+', style: TextStyle(fontSize: 24)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green, // Mudando para verde
+                ),
+              ),
+              ElevatedButton(
+                onPressed: subtrairViagem,
+                child: Text('-', style: TextStyle(fontSize: 24)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red, // Mudando para vermelho
+                ),
+              ),
+              ElevatedButton(
+                onPressed: zerar,
+                child: Text(
+                  'Zerar',
+                  style: TextStyle(fontSize: 24, color: Colors.white), // Alterado para branco
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[900], // Mudado para azul(900)
+                ),
+              ),
+            ],
           ),
           // Menu Inferior
           Expanded(
